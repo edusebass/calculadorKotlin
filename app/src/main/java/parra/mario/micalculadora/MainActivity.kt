9eb68a6a8803fe60a244c450fb631bdd9f97170b
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-07-30 20:16:38 -0500

    feat: funciones trogonometricas y delete all

--- app/src/main/java/parra/mario/micalculadora/MainActivity.kt
+++ app/src/main/java/parra/mario/micalculadora/MainActivity.kt
@@ -5,9 +5,12 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import kotlin.math.sin
+import kotlin.math.cos
+import kotlin.math.tan
 
 class MainActivity : AppCompatActivity() {
-    //0->nada; 1->suma; 2->resta; 3->mult; 4->div
+    // 0->nada; 1->suma; 2->resta; 3->mult; 4->div; 5->seno; 6->coseno; 7->tangente
     var oper: Int = 0
     var numero1: Double = 0.0
     lateinit var tv_num1: TextView
@@ -18,70 +21,89 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btnIgual: Button = findViewById(R.id.btnIgual)
-        val btnBorrar: Button = findViewById(R.id.btnBorrar)
+        val btnBorrar: Button = findViewById(R.id.btndeleteAll)
         tv_num1 = findViewById(R.id.tv_num1)
         tv_num2 = findViewById(R.id.tv_num2)
 
-        btnIgual.setOnClickListener{
-            var num2 = tv_num2.text.toString().toDouble()
+        btnIgual.setOnClickListener {
+            var num2 = tv_num2.text.toString().toDoubleOrNull() ?: 0.0
             var res = 0.0
-            when(oper){
-                1-> res = numero1 + num2
-                2-> res = numero1- num2
-                3-> res = numero1 * num2
-                4-> res = numero1 / num2
+            when (oper) {
+                1 -> res = numero1 + num2
+                2 -> res = numero1 - num2
+                3 -> res = numero1 * num2
+                4 -> res = numero1 / num2
+                5 -> res = sin(Math.toRadians(num2))
+                6 -> res = cos(Math.toRadians(num2))
+                7 -> res = tan(Math.toRadians(num2))
             }
             tv_num2.setText(res.toString())
             tv_num1.setText("")
         }
 
-        btnBorrar.setOnClickListener{
+        btnBorrar.setOnClickListener {
             tv_num1.setText("")
             tv_num2.setText("")
             oper = 0
         }
-
     }
 
-    fun clicNumero(view: View){
+    fun clicNumero(view: View) {
         var num2 = tv_num2.text.toString()
 
-        when(view.id){
-            R.id.btn0 -> tv_num2.setText(num2+"0")
-            R.id.btn1 -> tv_num2.setText(num2+"1")
-            R.id.btn2 -> tv_num2.setText(num2+"2")
-            R.id.btn3 -> tv_num2.setText(num2+"3")
-            R.id.btn4 -> tv_num2.setText(num2+"4")
-            R.id.btn5 -> tv_num2.setText(num2+"5")
-            R.id.btn6 -> tv_num2.setText(num2+"6")
-            R.id.btn7 -> tv_num2.setText(num2+"7")
-            R.id.btn8 -> tv_num2.setText(num2+"8")
-            R.id.btn9 -> tv_num2.setText(num2+"9")
-            R.id.btnPunto -> tv_num2.setText(num2+".")
+        when (view.id) {
+            R.id.btn0 -> tv_num2.setText(num2 + "0")
+            R.id.btn1 -> tv_num2.setText(num2 + "1")
+            R.id.btn2 -> tv_num2.setText(num2 + "2")
+            R.id.btn3 -> tv_num2.setText(num2 + "3")
+            R.id.btn4 -> tv_num2.setText(num2 + "4")
+            R.id.btn5 -> tv_num2.setText(num2 + "5")
+            R.id.btn6 -> tv_num2.setText(num2 + "6")
+            R.id.btn7 -> tv_num2.setText(num2 + "7")
+            R.id.btn8 -> tv_num2.setText(num2 + "8")
+            R.id.btn9 -> tv_num2.setText(num2 + "9")
+            R.id.btnPunto -> tv_num2.setText(num2 + ".")
         }
     }
 
-    fun clicOperacion(view: View){
-        var num2 = tv_num2.text.toString()
-        numero1 = num2.toString().toDouble()
+    fun clicOperacion(view: View) {
+        var num2 = tv_num2.text.toString().toDoubleOrNull() ?: 0.0
+        numero1 = num2
         tv_num2.setText("")
-        when(view.id){
+        when (view.id) {
             R.id.btnSumar -> {
-                tv_num1.setText(num2+"+")
+                tv_num1.setText(num2.toString() + "+")
                 oper = 1
             }
-            R.id.btnRestar ->{
-                tv_num1.setText(num2+"-")
+            R.id.btnRestar -> {
+                tv_num1.setText(num2.toString() + "-")
                 oper = 2
             }
-            R.id.btnMult ->{
-                tv_num1.setText(num2+"*")
+            R.id.btnMult -> {
+                tv_num1.setText(num2.toString() + "*")
                 oper = 3
             }
             R.id.btnDividir -> {
-                tv_num1.setText(num2+"/")
+                tv_num1.setText(num2.toString() + "/")
                 oper = 4
             }
+            R.id.btnSeno -> {
+                tv_num1.setText("sin($num2)")
+                oper = 5
+            }
+            R.id.btnCoseno -> {
+                tv_num1.setText("cos($num2)")
+                oper = 6
+            }
+            R.id.btnTangente -> {
+                tv_num1.setText("tan($num2)")
+                oper = 7
+            }
         }
+    }
+
+    fun onDeleteAll(view: View){
+        tv_num1.setText("")
+        tv_num2.setText("")
     }
 }
